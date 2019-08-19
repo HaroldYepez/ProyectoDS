8d071eb74b1f3f4a5237188ed0f859a15bd73ff4
Pedro Vera <pgvera@espol.edu.ec>
2019-08-18 21:31:32 -0500

    Base de datos, información para consultar.

--- src/Singleton/ConexionDB.java
+++ src/Singleton/ConexionDB.java
@@ -6,6 +6,9 @@
 package Singleton;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  *
@@ -13,13 +16,38 @@ import java.sql.Connection;
  */
 public class ConexionDB {
     private Connection coneccion;
+    private String user;
+    private String password;
+    private String db="";
+    private String host="localhost:8889";
+    private String server="jdbc:mysql://"+host+"/"+db;
 
     public Connection getConeccion() {
+        coneccion=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            coneccion=DriverManager.getConnection(server, user, password);
+        }catch(Exception e){
+            System.out.println(String.valueOf(e));    
+        }
         return coneccion;
     }
     
     public void conectar(){
         
+    }
+    
+    public ResultSet getTabla(String consulta){
+        coneccion=getConeccion();
+        ResultSet datos=null;
+        Statement st;
+        try{
+            st=coneccion.createStatement();
+            datos=st.executeQuery(consulta);
+        }catch(Exception e){
+            System.out.print(e.toString());
+        }
+        return datos;
     }
     
     public void cerrarConeccion(){
